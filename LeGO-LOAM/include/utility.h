0fa33aa5de753b7f60651674529dc1ea047a881b
zhenzhen.xiang <zhenzhen.xiang@lingzhitech.com>
2020-06-12 13:36:23 +0800

    modify horizon_scan number for RS-Lidar-32 and enable loop closure

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -105,13 +105,13 @@ extern const bool useCloudRing = true; // if true, ang_res_y and ang_bottom are 
 
 // RS-Lidar-32
 extern const int N_SCAN = 32;
-extern const int Horizon_SCAN = 1800;
+extern const int Horizon_SCAN = 2000;
 extern const float ang_res_x = 360.0/float(Horizon_SCAN);
 extern const float ang_res_y = 40.0/float(N_SCAN-1); // not used in ring mode
 extern const float ang_bottom = 25.0; // not used in ring mode
 extern const int groundScanInd = 20;
 
-extern const bool loopClosureEnableFlag = false;
+extern const bool loopClosureEnableFlag = true;
 extern const double mappingProcessInterval = 0.3;
 
 extern const float scanPeriod = 0.1;
